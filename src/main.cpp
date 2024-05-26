e612de060538c2fc135d9d6c10fee13da1dc2d20
Z1KO <Z1KOxl@pm.me>
2024-05-26 23:26:36 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+﻿#include "include/render.hpp"
+
+#include <thread>
+
+int __stdcall wWinMain(
+	HINSTANCE instance,
+	HINSTANCE previousInstance,
+	PWSTR arguments,
+	int commandShow)
+{
+	// create gui
+	render::CreateHWindow("Injector");
+	render::CreateDevice();
+	render::CreateImGui();
+
+	while (render::isRunning)
+	{
+		render::BeginRender();
+		render::Render();
+		render::EndRender();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
+
+	// destroy gui
+	render::DestroyImGui();
+	render::DestroyDevice();
+	render::DestroyHWindow();
+
+	return EXIT_SUCCESS;
+}
